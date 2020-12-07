3f345406c613f0376170c4aa3c7ebd1c32994ff0
RohanK22 <rohank2205@gmail.com>
2020-12-07 10:07:57 +0000

    Finished graphing AAPL stock data

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 const Chart = require('chart.js');
+const unix = require('to-unix-timestamp');
 
   class Header extends React.Component {
     render() {
@@ -23,11 +24,37 @@ const Chart = require('chart.js');
     async componentDidMount() {
       const apiKey = 'bv184tv48v6p0f6idl20';
       const symbol = 'AAPL';
-      const response = await fetch(`https://finnhub.io/api/v1/quote?symbol=` + symbol + `&token=` + apiKey);
+      const startDate = Date.parse('01 Jan 1970 00:00:00 GMT');
+      const endDate = unix(Date.now());
+      const response = await fetch(`https://finnhub.io/api/v1/stock/candle?symbol=` + symbol + `&resolution=60&from=` + startDate + `&to=`+ endDate +`&token=` + apiKey);
       const json = await response.json();
       console.log(json);
       this.setState({ realTimeData: json });
       this.update();
+
+      // chart
+      var ctx = document.getElementById('myChart').getContext('2d');
+      var chart = new Chart(ctx, {
+          // The type of chart we want to create
+          type: 'line',
+
+          // The data for our dataset
+          data: {
+              labels: this.state.realTimeData.t,
+              datasets: [{
+                  label: 'AAPL',
+                  backgroundColor: 'rgb(255, 99, 132)',
+                  borderColor: 'rgb(255, 99, 132)',
+                  borderWidth: 1,
+                  data: this.state.realTimeData.c,
+                  fill: false
+              }]
+          },
+
+          // Configuration options go here
+          options: {}
+      });
+      console.log( chart);
     }
 
     update() {
