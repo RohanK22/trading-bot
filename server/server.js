300fde2d10e953d6526b2fc764b47451e26edbe0
RohanK22 <rohank2205@gmail.com>
2020-12-10 16:53:05 +0000

    Added express back-end

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('../client/build'));
+
+// app.get('/', (req, res) => {
+//     console.log('Got a get request');
+// });
+
+const port = process.env.PORT || 1337;
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
