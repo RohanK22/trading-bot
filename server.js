d588eb060d6c0c567a7bb1d5450ecfb57eca7f23
RohanK22 <rohank2205@gmail.com>
2020-12-21 19:59:02 +0000

    Added a /ping request to the server

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('/ping', (req, res) => {
+  console.log('got a /ping request');
+  res.json({
+    message: 'Pong',
+  });
 });
 
 app.listen(process.env.PORT || 9000);
