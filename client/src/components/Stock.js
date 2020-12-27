2534e01df16814094e09897803d8d71c6f3f76f1
RohanK22 <rohank2205@gmail.com>
2020-12-27 18:28:36 +0000

    Added Stock Component State

--- client/src/components/Stock.js
+++ client/src/components/Stock.js
@@ -3,6 +3,29 @@ import React, { Component } from 'react';
 class Stock extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            symbol: '',
+            currentPrice: 0.0,
+            highPriceDay: 0.0,
+            lowPriceDay: 0.0,
+            openPriceDay: 0.0,
+            previousDayClosePrice: 0.0,
+            mic: '', // which stock index it corresponds to
+            marketOpen: false,
+            historyDay: null,
+            historyWeek: null,
+            historyMonth: null,
+            historyHalfYear: null,
+            historyYear: null,
+            historyAllTime: null,
+        };
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row"></div>
+            </div>
+        );
     }
 }
