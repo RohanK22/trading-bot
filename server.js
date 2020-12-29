8dfc7e81a2a672b43928d6d42d8b95667adc79a8
RohanK22 <rohank2205@gmail.com>
2020-12-29 22:04:11 +0000

    Removed git / Request

--- server.js
+++ server.js
@@ -5,18 +5,14 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 app.get('/ping', (req, res) => {
-  console.log('got a /ping request');
-  res.json({
-    message: 'Pong',
-  });
+    console.log('got a /ping request');
+    res.json({
+        message: 'Pong',
+    });
 });
 
 let port = process.env.PORT || 9000;
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
